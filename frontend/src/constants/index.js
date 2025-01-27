061f36727807ebc375458546c16a45b05e64b537
CyndieKamau <cyndiekamaa@gmail.com>
2025-01-27 04:25:39 +0300

    feat: Header section frontend v1

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,33 @@
+export const navigation = [
+    {
+      id: "0",
+      title: "home",
+      url: "#hero",
+    },
+    {
+      id: "1",
+      title: "Storyline",
+      url: "#storyline",
+    },
+    {
+      id: "2",
+      title: "Artifacts",
+      url: "#artifacts",
+    },
+    {
+      id: "3",
+      title: "Characters",
+      url: "#characters",
+    },
+    {
+      id: "4",
+      title: "Gameplay",
+      url: "#gameplay",
+    },
+    {
+      id: "5",
+      title: "Pre-register",
+      url: "#pre-register",
+      onlyMobile: true,
+    },
+  ];
